01069366e98590450cd9b49447c5b53eae75aafe
Ed Cranford <ed.cranford@rackspace.com>
2014-06-10 15:36:15 -0500

    Readding GIT_URL

--- tester/test.sh
+++ tester/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$GIT_URL" ]; then
+  echo GIT_URL unspecified
+        exit 1
+fi
+
 if [ -z "$TEST_CMD" ]; then
   echo TEST_CMD unspecified
         exit 1
@@ -7,6 +12,7 @@ fi
 
 app_dir=/app
 mkdir -p $app_dir
-cat | tar -xC $app_dir
+git clone $GIT_URL $app_dir
+#cat | tar -xC $app_dir
 pushd $app_dir
 $TEST_CMD
